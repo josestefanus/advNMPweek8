269dfaf9f7bfc5a5a738b9873c1992cf9422804f
josestefanus <s160419044@student.ubaya.ac.id>
2021-10-14 15:29:01 +0700

    init

---
+++ app/src/main/java/id/ac/ubaya/informatika/todoapp/viewmodel/DetailTodoViewModel.kt
@@ -0,0 +1,28 @@
+package id.ac.ubaya.informatika.todoapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.room.Room
+import id.ac.ubaya.informatika.todoapp.model.Todo
+import id.ac.ubaya.informatika.todoapp.model.TodoDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class DetailTodoViewModel(application: Application):AndroidViewModel(application), CoroutineScope {
+    private val job = Job()
+
+    fun addTodo(todo:Todo) {
+        launch {
+            val db = Room.databaseBuilder(getApplication(),
+                TodoDatabase::class.java, "tododb").build()
+            db.todoDao().insertAll(todo)
+        }
+    }
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+}
